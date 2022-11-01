4d01b732d65af7bbb0af45bb96300bdc31fa4cd1
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-01 13:07:12 -0300

    chore: setup fastify

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify';
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    fastify.get('/pools/count', () => {
+        return { count: 0 }
+    })
+
+    await fastify.listen({ port: 3333 });
+};
+
+bootstrap();
