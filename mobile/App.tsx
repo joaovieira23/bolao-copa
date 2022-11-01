d0426fe383b4de5e32065acf861559e44b7abb54
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-01 18:16:14 -0300

    feat: create loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Center, Text } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading  } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+
+  
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          Hello React Native
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
