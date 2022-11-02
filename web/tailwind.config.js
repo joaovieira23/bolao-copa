8d7615e70aa32ae4352305cafd5a8c8cff943082
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-02 19:44:38 -0300

    feat: finish web screen

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,8 +9,25 @@ module.exports = {
         sans: 'Roboto, sans-serif'
       },
 
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+
       colors: {
+        ignite: {
+          500: '#129E57',
+        },
+
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D',
+        },
+
         gray: {
+          100: '#E1E1E6',
+          300: '#8d8d99',
+          600: '#323238',
+          800: '#202024',
           900: "#121214"
         }
       }
