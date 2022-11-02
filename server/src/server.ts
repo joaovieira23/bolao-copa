32e582f5cd272c2bd7b90401ec8203d171fff3f9
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-02 17:40:27 -0300

    feat: create pool endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
+import ShortUniqueId from 'short-unique-id';
+import { getPackedSettings } from 'http2';
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -32,6 +35,30 @@ async function bootstrap() {
         const count = await prisma.pool.count();
 
         return { count }
+    })
+
+    fastify.post('/pools', async (request, response) => {
+
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        // Zod casa muito bem com Typescript pois dizendo que o title é uma string, 
+        // não preciso nem mais fazer a typagem novamente dela
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 })
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return response.status(201).send({ code });
     })
 
     await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
