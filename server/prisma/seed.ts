cfbff21ad15059e80edb6b267e207499e3fafed4
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-02 17:16:42 -0300

    feat: create database seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Andrade',
+            email: 'john.andrade@gmail.com',
+            avatarUrl: 'https://github.com/joaovieira23.png',
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-02T12:00:00.201Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-03T12:00:00.201Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 0,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+            
+        },
+    })
+
+    // const participant = await prisma.participant.create({
+    //     data: {
+    //         poolId: pool.id,
+    //         userId: user.id,
+    //     }
+    // })
+}
+
+main();
