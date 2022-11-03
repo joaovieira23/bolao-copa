204aa01eae173d020dc5172dcca6a2bd3fa0a046
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-03 00:01:15 -0300

    feat: terminate a login interface

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -6,13 +6,18 @@ import { Button } from '../components/Button';
 
 export function SignIn() {
     return (
-      <Center flex={1} bgColor="gray.900">
+      <Center flex={1} bgColor="gray.900" p={7}>
         <Logo  width={212} height={40} />
         <Button 
-          title='ENTRAR COM GOOGLE'
           type='SECONDARY'
+          title='ENTRAR COM GOOGLE'
           leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+          mt={12}
         />
+
+        <Text color="white" textAlign="center" mt={4}>
+          Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+        </Text>
       </Center>
     )
 }
