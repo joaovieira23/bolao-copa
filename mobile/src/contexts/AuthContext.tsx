4fe1215822a72a6b500fc463c3364cb3089dad4c
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-03 10:25:04 -0300

    feat: implement google authentication

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -8,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
@@ -20,16 +25,41 @@ export const AuthContext = createContext({
 } as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<UserProps>({} as UserProps);
+    const [isUserLoading, setIsUserLoading] = useState(false);
 
-    console.log(AuthSession.makeRedirectUri({ useProxy: true }));
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '569012611706-r7llbeuv1j0k9qh015qcr3kfcjaloae0.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    });
 
     async function signIn() {
-        console.log('Logou!');
+        try {
+            setIsUserLoading(true);
+            await promptAsync();
+        } catch (error) {
+            console.log('error', error);
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
     };
+
+    async function signInWithGoogle(access_token: string) {
+        console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token)
+    };
+
+    useEffect(() => {
+        if(response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken);
+        }
+    }, [response]);
 
     return (
         <AuthContext.Provider value={{
             signIn,
+            isUserLoading,
             user: {
                 name: 'João Andrade',
                 avatarUrl: 'https://github.com/joaovieira23.png'
