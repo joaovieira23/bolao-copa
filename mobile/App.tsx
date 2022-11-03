266ed04eed063a7342d1f2aba1cf905eedef2aac
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-03 14:41:24 -0300

    feat: create components mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { NativeBaseProvider, StatusBar} from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/SignIn';
+// import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
 import { Loading  } from './src/components/Loading';
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
@@ -20,7 +21,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <New /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
