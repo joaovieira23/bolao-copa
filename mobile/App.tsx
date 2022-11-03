092717895a289b2c4a16319d96f263521e4401e2
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-03 16:36:42 -0300

    feat: finish find interface

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,13 @@ import { NativeBaseProvider, StatusBar} from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 // import { SignIn } from './src/screens/SignIn';
-import { New } from './src/screens/New';
+// import { New } from './src/screens/New';
 import { Loading  } from './src/components/Loading';
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
 import { THEME } from './src/styles/theme';
+import { Find } from './src/screens/Find';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
@@ -21,7 +22,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        {fontsLoaded ? <New /> : <Loading />}
+        {fontsLoaded ? <Find /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
