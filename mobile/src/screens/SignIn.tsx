2aa7d8be0673a4e0bde442f7a9ec9662f18d84a0
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-02 22:59:44 -0300

    feat: create button component

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,18 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
 
 export function SignIn() {
     return (
       <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24} fontFamily="heading">
-            SignIn
-        </Text>
+        <Logo  width={212} height={40} />
+        <Button 
+          title='ENTRAR COM GOOGLE'
+          type='SECONDARY'
+          leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        />
       </Center>
     )
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,8 +3,8 @@ import appPreviewImg from '../assets/app-nlw-copa-preview.png';
 import logoImg from '../assets/logo.svg';
 import usersAvatarExampleImg from '../assets/users-avatar-example.png';
 import iconCheckImg from '../assets/icon-check.svg';
-import { FormEvent, useState } from "react";
 import { api } from "../lib/axios";
+import { FormEvent, useState } from "react";
 
 interface HomeProps {
   poolCount: number;
@@ -13,6 +13,29 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle
+      })
+
+      const { code } = response.data;
+
+      await navigator.clipboard.writeText(code);
+
+      alert('Bolão criado com sucesso, o código fiu copiado para a área de transferência!');
+
+      setPoolTitle('')
+    } catch (err) {
+      console.log(err);
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
@@ -30,12 +53,14 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input
             className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text"
             required
             placeholder="Qual nome do seu bolão?"
+            value={poolTitle}
+            onChange={event => setPoolTitle(event.target.value)}
           />
           <button
             className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
@@ -63,7 +88,7 @@ export default function Home(props: HomeProps) {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+6.423</span>
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
               <span>Palpites enviados</span>
             </div>
           </div>
@@ -78,7 +103,6 @@ export default function Home(props: HomeProps) {
     </div>
   )
 }
-
 
 export const getServerSideProps = async () => {
   const [
