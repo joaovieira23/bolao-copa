ed3a1f41c7810cab246bfc0ed2422db178240f17
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-04 12:18:31 -0300

    feat: list created pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,13 +1,45 @@
-import { VStack, Icon } from 'native-base';
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
+    const[isLoading, setIsLoading] = useState(true);
+    const[pools, setPools] = useState<PoolCardProps[]>([]);
 
     const navigation = useNavigation();
+
+    const toast = useToast();
+
+    async function fetchPools() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/pools');
+            setPools(response.data.pools);
+        } catch(error) {
+            console.log('error', error);
+
+            toast.show({
+                title: 'Não foi possível carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchPools();
+    }, [])
 
     return (
         <VStack flex={1} bgColor="gray.900">
@@ -19,6 +51,19 @@ export function Pools() {
                     onPress={() =>  navigation.navigate('find')}
                 />
             </VStack>
+
+            {
+                isLoading ? <Loading /> : 
+                <FlatList 
+                    data={pools}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <PoolCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    ListEmptyComponent={() => <EmptyPoolList />}
+                    _contentContainerStyle={{ pb: 10 }}
+                    px={5}
+                />
+            }
         </VStack>
     )
 }
