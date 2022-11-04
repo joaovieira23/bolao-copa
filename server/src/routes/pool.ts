68067ead575157db857417e8df471fe7410d2227
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-03 22:11:04 -0300

    feat: create pool details endpoint

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -56,7 +56,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
         return response.status(201).send({ code });
     });
 
-    fastify.post('/pools/:id/join', {
+    fastify.post('/pools/join', {
         onRequest: [authenticate]
     }, async (request, response) => {
         const joinPoolBody = z.object({
@@ -150,5 +150,48 @@ export async function poolRoutes(fastify: FastifyInstance) {
         })
 
         return { pools };
+    })
+
+    fastify.get('/pools/:id', {
+        onRequest: [authenticate],
+    }, async (request) => {
+        const getPoolParams = z.object({
+            id: z.string(),
+        })
+
+        const { id } = getPoolParams.parse(request.params);
+
+        const pool = await prisma.pool.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                _count: {
+                    select: {
+                        participants: true
+                    }
+                },
+                participants: {
+                    select: {
+                        id: true,
+
+                        user: {
+                            select: {
+                                avatarUrl: true
+                            }
+                        }
+                    },
+                    take: 4
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            }
+        })
+
+        return { pool }
     })
 }
