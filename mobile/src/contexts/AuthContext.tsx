137308a78c54e4396152cc8ac2448e5fb8cfeccd
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-04 10:50:35 -0300

    feat: fetch user data

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -47,7 +48,22 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     };
 
     async function signInWithGoogle(access_token: string) {
-        console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token)
+        try {
+            setIsUserLoading(true);
+
+            const tokenResponse = await api.post('/users', { access_token });
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
+
+            const userInfoResponse = await api.get('/me');
+
+            setUser(userInfoResponse.data.user);
+        } catch(error) {
+            console.log('error', error);
+            throw error;
+        } finally {
+            setIsUserLoading(false);
+        }
     };
 
     useEffect(() => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
     
 
-    await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
 };
 
 bootstrap();
