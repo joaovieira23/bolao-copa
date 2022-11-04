36eaeaa3470494401600e2fac410d387d96690f9
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-04 08:29:15 -0300

    feat: list of pool games

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,5 +1,44 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function gameRoutes(fastify: FastifyInstance) {
+    fastify.get('/pools/:id/games', {
+        onRequest: [authenticate],
+    }, async (request) => {
+        const getPoolParams = z.object({
+            id: z.string(),
+        })
+
+        const { id } = getPoolParams.parse(request.params);
+
+
+        //Relacionamento de palpites onde o participante tenha o id do usuário logado e o id do bolão, seja o id do parâmetro
+        const games = await prisma.game.findMany({
+            orderBy: {
+                date: 'desc'
+            },
+            include: {
+                guesses: {
+                    where: {
+                        participant: {
+                            userId: request.user.sub,
+                            poolId: id
+                        }
+                    }
+                }
+            }
+        })
+
+        return { 
+            games: games.map(game => {
+                return {
+                    ...game,
+                    guess: game.guesses.length > 0 ? game.guesses[0] : null,
+                    guesses: undefined
+                }
+            })
+        };
+    })
 }
